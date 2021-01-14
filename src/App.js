5828925852d37370cd5eeccfa5a028e6af7b060f
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-14 15:14:43 +0700

    init Project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import TodoPage from "./container/TodoPage"
+import ContactPage from "./container/ContactPage"
+
+function App() {
+  return (
+    <div>
+      <TodoPage />
+      <ContactPage />
+    </div>
+  );
+}
+
+export default App;
